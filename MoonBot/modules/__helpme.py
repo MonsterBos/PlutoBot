7e9a2164bf2d7d189f28c43439f3795d38cba2f4
MonsterBos <122009166+MonsterBos@users.noreply.github.com>
2023-12-18 19:29:39 +0300

    Update __helpme.py

--- MoonBot/modules/__helpme.py
+++ MoonBot/modules/__helpme.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# OwenUserBot - jackdanielssx
+# MoonBot - jackdanielssx
 
 # @Qulec tarafından yazılmıştır.
 # Thanks @Spechide.
